c13221e0cc0e1352a0145265497aada9f4302b7a
kutteai <kutteai978@gmail.com>
2025-07-13 23:18:33 +0100

    a

--- labmyshare/settings.py
+++ labmyshare/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Static files storage
