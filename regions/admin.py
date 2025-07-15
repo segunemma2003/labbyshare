784830e15021b0e6c61bb2ba1a9f656105454109
kutteai <kutteai978@gmail.com>
2025-07-15 11:49:18 +0100

    a

--- regions/admin.py
+++ regions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from regions.models import Region, RegionalSettings
+
 # Register your models here.
+admin.sites.register(Region)
+admin.sites.register(RegionalSettings)
