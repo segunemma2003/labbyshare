3a590b1d3407d68e28eee64e5c4948b4234408a2
kutteai <kutteai978@gmail.com>
2025-07-15 11:57:15 +0100

    a

--- professionals/admin.py
+++ professionals/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Professional, ProfessionalAvailability, ProfessionalDocument, ProfessionalRegion, ProfessionalService, ProfessionalUnavailability
+
 # Register your models here.
+admin.sites.register(Professional)
+admin.sites.register(ProfessionalRegion)
+admin.sites.register(ProfessionalService)
+
+admin.sites.register(ProfessionalAvailability)
+admin.sites.register(ProfessionalUnavailability)
+admin.sites.register(ProfessionalDocument)
--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import AddOn, Category, RegionalPricing, Service, ServiceImage, ServiceReview
+
 # Register your models here.
+admin.sites.register(Category)
+admin.sites.register(Service)
+admin.sites.register(RegionalPricing)
+admin.sites.register(AddOn)
+admin.sites.register(ServiceImage)
+admin.sites.register(ServiceReview)
