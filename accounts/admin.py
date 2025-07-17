b162eb5b92a41df1c88f24dbb74e504905992c31
kutteai <kutteai978@gmail.com>
2025-07-17 11:41:02 +0100

    a

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Register all models in this app
+for model in [m for name, m in globals().items() if hasattr(m, '__module__') and m.__module__ == 'accounts.models']:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
